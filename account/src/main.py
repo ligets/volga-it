682e5eed3372beb9ffab7e86e0c625077ed1759c
ligets <legen2a208@gmail.com>
2024-10-25 13:37:58 +0400

    -reload and info account service

--- account/src/main.py
+++ account/src/main.py
@@ -56,4 +56,4 @@ app.add_middleware(
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8081, log_level='info', reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8081, log_level='error')
